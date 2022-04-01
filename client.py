2544d451a5da196b1a8136bab9e5ecc51a93a69f
PBBond <97637272+PBBond@users.noreply.github.com>
2022-04-01 14:37:58 -0500

    username and password checks

--- client.py
+++ client.py
@@ -1,31 +1,48 @@
 import socket
 import threading
 
+# DO NOT DELETE, WILL NEED LATER
+#check for correct number of args
+# if (len(sys.argv) != 4):
+#     print("Incorrect number of arguments")
+#     print("Use: client_simple_udp.py <Server Host> <Port> <username>")
+#     sys.exit()
+
+#IP = sys.argv[1]
+IP = '127.0.0.1'
+#PORT = int(sys.argv[2])
+PORT = 5000
+#userName = sys.argv[3]
+#create socket and catch errors
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1',55555))
+client.connect((IP, PORT))
+FMT = 'utf-8'
 
-def recieve():
+def receive():
+
+    #handle username and password
     while True:
-        try:
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                pass
-            else:
-                print(message)
-        except:
-            print("An error occured!")
-            client.close()
+        # client.send(userName.encode(FMT)) # use this later
+        client.send(input("Enter username: ").encode(FMT)) # delete this later
+        client.send(input("Enter password: ").encode(FMT))
+        message = client.recv(1024).decode(FMT)
+        if message != 'Password incorrect':  # if password is correct break out of loop
+            print(message)
             break
+        else:
+            print("Password incorrect, try again")
+    print('Options:')
+    print("PM: Public Message, DM: Direct Messaging, EX: Exit")
+    op = input("Enter Option: ")
 
-def write():
-    while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+    while(op != 'EX'):
+        if op == "PM":
+            return # FIXME
 
-receive_thread = threading.Thread(target=recieve)
+
+
+
+receive_thread = threading.Thread(target=receive)
 receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
-
 
--- server.py
+++ server.py
@@ -1,66 +1,87 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-   # print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
 import threading
 import socket
 
-host = '127.0.0.1' # localhost
-port = 55555
+# DO NOT DELETE, WILL NEED LATER
+#check for correct number of args
+# if (len(sys.argv) != 2):
+#     print("Incorrect number of arguments")
+#     print("Use: server_simple_udp.py <Port>")
+#     sys.exit()
+#port = int(sys.argv[1])  # port is an argument
 
+host = '127.0.0.1' # localhost
+port = 55555 #for now, using hardcoded port
+FMT = 'utf-8'
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
 clients = []
-nicknames = []
 
 def broadcast(message):
     for client in clients:
         client.send(message)
 
+def checkUsernames(user):
+    with open('users.txt', 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        line = line.split(' ')
+        print(f"stuff: {user} {line[0]}")
+        if user == line[0]:
+            f.close()
+            return True
+    f.close()
+    return False
+
+def checkPassword(user, passWord):
+    with open('users.txt', 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        line = line.split(' ')
+        if user == line[0]:
+            if passWord == line[1]:
+                f.close()
+                return True
+            else:
+                f.close()
+                return False
+    f.close()
+    return False
+
+def createUser(userName, passWord):
+    with open('users.txt', 'a') as f:
+        f.write(f'{userName} {passWord}\n')
+
 def handle(client):
+    #first handle username/password situation
     while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames( index)
-            broadcast(f'{nickname}) left the chat!'.encode('ascii'))
-            nicknames.remove(nickname)
+        userName = client.recv(1024).decode(FMT)
+        passWord = client.recv(1024).decode(FMT)
+        if checkUsernames(userName): # see if userName exists
+            if not checkPassword(userName, passWord): # check if password matches
+                client.send("Password incorrect".encode(FMT))
+            else:
+                client.send("Password correct".encode(FMT))
+                break
+        else: # username doesn't exist, created new username
+            createUser(userName, passWord)
+            client.send("Created new user".encode(FMT))
             break
 
 
-def recieve():
+
+def receive():
+    print("Server is ready to receive on port ", port)
     while True:
-        client, address =server.accept()
+        print('Waiting...')
+        client, address = server.accept()
         print(f"Connected with {str(address)}")
-
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('asci(i')
-        nicknames.append(client)
-
-        print(f'Nickname of client is {nickname}!')
-        broadcast(f'{nickname} joined the chat'.encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        clients.append(client)
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-recieve()
+receive()
